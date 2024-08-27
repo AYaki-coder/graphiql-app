29f0ab10d532bfad212b3528a9e16cb3bc984ecd
siviavio4ka <siviavio4ka@gmail.com>
2024-08-27 21:18:32 +0300

    feat: add config for vitest

--- graphiql-app/vite.config.ts
+++ graphiql-app/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -13,4 +16,13 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    coverage: {
+      provider: 'v8',
+      ignoreEmptyLines: false,
+    },
+  },
 });
