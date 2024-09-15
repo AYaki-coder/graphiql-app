7bd076e6f7bf5d0f79d8e6d84ab22e743331ddfb
siviavio4ka <siviavio4ka@gmail.com>
2024-09-15 22:13:49 +0300

    fix: typo

--- app/utils/formatterGraphQL.ts
+++ app/utils/formatterGraphQL.ts
@@ -73,39 +73,3 @@ export function formatGraphQL(query: string): string {
 
   return formatGraphQL2(formattedQuery);
 }
-
-// export function formatGraphQL(query: string): string {
-//   query = query.trim();
-
-//   query = query.replace(/\s*{\s*/g, ' {\n').replace(/\s*}\s*/g, '\n}');
-
-//   const lines = query.split('\n');
-//   let indentLevel = 0;
-//   const indentSpace = '  ';
-//   const formattedLines = [];
-
-//   for (let line of lines) {
-//     line = line.trim();
-
-//     if (line === '') continue;
-
-//     if (line.startsWith('}')) {
-//       indentLevel--;
-//     }
-
-//     const words = line.split(/\s+/);
-//     const formattedLine = words
-//       .map((word, index) => (index === 0 ? word : '\n' + indentSpace.repeat(indentLevel + 1) + word))
-//       .join('');
-
-//     formattedLines.push(indentSpace.repeat(indentLevel) + formattedLine);
-
-//     if (line.endsWith('{')) {
-//       indentLevel++;
-//     }
-//   }
-
-//   const formattedQuery = format(formattedLines.join('\n'));
-
-//   return formattedQuery.replace(/\t/g, '  ');
-// }
