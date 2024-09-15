6b6dfd45b317f50407c518af1c0dc3f11415e236
AV-Shell <avshell.sj@gmail.com>
2024-09-15 06:06:51 +0300

    feat: add catching errors, add validation

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -1,9 +1,16 @@
 import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
-import { Link, Form, redirect } from '@remix-run/react';
+import _ from 'lodash';
+import { Link, Form, redirect, json, useActionData, useNavigation } from '@remix-run/react';
 import { firebaseSignIn } from '~/firebase/firebase';
 import { createUserSession, getUserSession } from '~/firebase/session';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useSubmit } from '@remix-run/react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import s from '../styles/signin-signout.module.scss';
 import classNames from 'classnames';
+import { signSchema } from '~/utils/schema';
+import { ISignValues } from '~/models/sing';
+import { useRef } from 'react';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const sessionUser = await getUserSession(request);
@@ -18,39 +25,91 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
   const email = formData.get('email');
   const password = formData.get('password');
-
-  const { user } = await firebaseSignIn(String(email ?? ''), String(password ?? ''));
-  const token = await user.getIdToken();
-  return createUserSession(token, '/');
+  try {
+    const { user } = await firebaseSignIn(String(email ?? ''), String(password ?? ''));
+    const token = await user.getIdToken();
+    return createUserSession(token, '/');
+  } catch (error) {
+    return json({ error: `${_.get(error, 'message', '')}` });
+  }
 };
 
 export default function Login() {
+  const formRef = useRef<HTMLFormElement>(null);
+  const submit = useSubmit();
+  const navigation = useNavigation();
+  const { register, handleSubmit, formState } = useForm({
+    mode: 'all',
+    resolver: yupResolver(signSchema),
+    defaultValues: {},
+  });
+  let errorMessage = '';
+  const data = useActionData<typeof action>();
+  if (data && data.error) {
+    errorMessage = data.error;
+  }
+
+  const onSubmit: SubmitHandler<ISignValues> = async (data: ISignValues) => {
+    try {
+      if (formRef.current) {
+        submit(formRef.current);
+      }
+    } catch (error) {
+      console.log(_.get(error, 'message', ''));
+    }
+  };
+
   return (
     <div className="login">
-      <h1>Login Page</h1>
+      <h1>SignIn Page</h1>
 
-      <Form method="post">
-        <p>
-          <label>
-            Email
-            <input className={classNames(s.emailInput, s.formControl)} type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Password
-            <input className={classNames(s.passwordInput, s.formControl)} type="password" name="password" />
-          </label>
-        </p>
+      <Form
+        method="post"
+        ref={formRef}
+        className={s.form}
+        //https://github.com/facebook/react/issues/30745
+        // eslint-disable-next-line react-compiler/react-compiler
+        onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">
+          Email: <span className={s.errorMessage}>{formState.errors.email?.message}</span>
+        </label>
+        <input
+          className={classNames(s.emailInput, s.formControl)}
+          type="text"
+          id="email"
+          {...register('email')}
+          placeholder="Enter your email"
+        />
+        <label htmlFor="password">
+          Password: <span className={s.errorMessage}>{formState.errors.password?.message}</span>
+        </label>
+        <input
+          className={classNames(s.emailInput, s.formControl)}
+          type="password"
+          id="password"
+          {...register('password')}
+          placeholder="Enter your password"
+        />
+        <div
+          className={classNames(s.backendErrorMessage, {
+            [s.hidden]: !errorMessage || navigation.state === 'submitting',
+          })}>
+          <span className={s.errorMessage}>{errorMessage}</span>
+        </div>
 
-        <button type="submit" className={classNames(s.btnPrimary, s.btn)}>
-          Login
-        </button>
+        <div className={s.buttonsContainer}>
+          <button
+            type="submit"
+            className={classNames(s.btnPrimary, s.btn)}
+            disabled={!formState.isValid || navigation.state !== 'idle'}>
+            Login
+          </button>
+
+          <Link to="/signup" className={classNames(s.btnLight, s.btn)}>
+            Go to Registration
+          </Link>
+        </div>
       </Form>
-
-      <Link to="/signup" className={classNames(s.btnLight, s.btn)}>
-        Create Account
-      </Link>
     </div>
   );
 }
