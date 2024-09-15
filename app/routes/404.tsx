315dee7d4ca71d04202715024053441c7d01875f
ayaki-coder <sj.a.yakimovich@gmail.com>
2024-09-16 00:06:39 +0300

    feat: restyle and add 2 languages

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -1,7 +1,16 @@
-export default function History() {
+import { useContext } from 'react';
+import s from '../styles/not-found.module.scss';
+import { LangContext } from '~/components/lang-context/lang-context';
+
+export default function NotFoundPage() {
+  const { langRecord } = useContext(LangContext);
   return (
-    <div className="404page">
-      <h1>Page 404! You are welcome!</h1>
+    <div className={s.page404}>
+      <div className={s.h404}>404</div>
+      <div>
+        <h1 className={(s.display1, s.textWhite)}>{langRecord.page404.heading}</h1>
+        <p>{langRecord.page404.text}</p>
+      </div>
     </div>
   );
 }
